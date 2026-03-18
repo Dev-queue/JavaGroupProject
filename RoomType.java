be870ae56efbd045f3a492eea71fe64994443d07
ginevra de'Benci <muharm809@gmail.com>
2026-03-18 14:15:57 +0200

    Add files via upload

---
+++ RoomType.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hotelmanagemnetsystem;
+
+/**
+ *
+ * @author mohar
+ */
+public enum RoomType {
+    SINGLE,
+    DOUBLE,
+    TWIN,
+    TRIPLE,
+    SUITE,
+    DELUXE,
+    PENTHOUSE,
+    FAMILY,
+    STUDIO,
+    PRESIDENTIAL
+}
